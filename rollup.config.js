6d41955f929ff5786443d28b9ce9c4408087795e
calebtuttle <1calebtuttle@gmail.com>
2022-08-08 19:41:54 -0400

    Use rollup for bundling. Buggy

---
+++ rollup.config.js
@@ -0,0 +1,18 @@
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+
+export default {
+  // Content scripts
+  input: "./src/content/content_script.js",
+  output: {
+    file: "./dist/content_script.js",
+    format: "es",
+  },
+  plugins: [
+    resolve({
+      browser: true,
+      preferBuiltins: true,
+    }),
+    commonjs(),
+  ],
+};
