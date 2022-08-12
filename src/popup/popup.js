e21b1ed9cf1db6af7bd3cb198a35a5f41531be4b
calebtuttle <1calebtuttle@gmail.com>
2022-08-12 19:20:48 -0400

    Add change-password form

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -3,12 +3,26 @@ import { CryptoController } from "./CryptoController";
 window.onload = function () {
   const cryptoController = new CryptoController();
 
-  const formEl = document.getElementById("register-form");
-  formEl.onsubmit = async (event) => {
+  const registerFormEl = document.getElementById("register-form");
+  registerFormEl.onsubmit = async (event) => {
     event.preventDefault();
     const password = event.target.password.value;
     const loginSuccess = await cryptoController.login(password);
     console.log(`loginSuccess: ${loginSuccess}`);
-    formEl.password.value = "";
+    registerFormEl.password.value = "";
+  };
+
+  const changePwFormEl = document.getElementById("change-password-form");
+  changePwFormEl.onsubmit = async (event) => {
+    event.preventDefault();
+    const oldPassword = event.target["old-password"].value;
+    const newPassword = event.target["new-password"].value;
+    const changePwSuccess = await cryptoController.changePassword(
+      oldPassword,
+      newPassword
+    );
+    console.log(`changePwSuccess: ${changePwSuccess}`);
+    changePwFormEl["old-password"].value = "";
+    changePwFormEl["new-password"].value = "";
   };
 };
