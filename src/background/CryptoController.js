3d9127556c884310d8ac7af66cf95e7d286fe05b
calebtuttle <1calebtuttle@gmail.com>
2022-08-13 20:33:44 -0400

    Fix decryption function

--- src/background/CryptoController.js
+++ src/background/CryptoController.js
@@ -133,30 +133,27 @@ class CryptoController {
   }
 
   async decryptWithPrivateKey(message) {
-    try {
-      const algo = {
-        name: "RSA-OAEP",
-        modulusLength: 4096,
-        publicExponent: new Uint8Array([1, 0, 1]),
-        hash: "SHA-256",
-      };
-      let args = ["jwk", this.#store.privateKey, algo, false, ["decrypt"]];
-      const privateKeyAsCryptoKey = await crypto.subtle.importKey(...args);
-      const encoder = new TextEncoder();
-      const encodedMessage = encoder.encode(message);
-      args = [{ name: "RSA-OAEP" }, privateKeyAsCryptoKey, encodedMessage];
-
-      // TODO: An error throws here. Figure out the problem.
-      const decryptedMessage = await crypto.subtle.decrypt(...args);
-
-      console.log("decryptWithPrivateKey: Reached line 153");
-      const decoder = new TextDecoder("utf-8");
-      console.log("decryptWithPrivateKey: Reached line 155");
-      return decoder.decode(decryptedMessage);
-    } catch (err) {
-      console.log("decryptWithPrivateKey error...");
-      console.log(err);
-    }
+    const algo = {
+      name: "RSA-OAEP",
+      modulusLength: 4096,
+      publicExponent: new Uint8Array([1, 0, 1]),
+      hash: "SHA-256",
+    };
+    const privateKeyAsCryptoKey = await crypto.subtle.importKey(
+      "jwk",
+      this.#store.privateKey,
+      algo,
+      false,
+      ["decrypt"]
+    );
+    const encodedMessage = new Uint8Array(JSON.parse(message)).buffer;
+    const decryptedMessage = await crypto.subtle.decrypt(
+      { name: "RSA-OAEP" },
+      privateKeyAsCryptoKey,
+      encodedMessage
+    );
+    const decoder = new TextDecoder("utf-8");
+    return decoder.decode(decryptedMessage);
   }
 
   /**
