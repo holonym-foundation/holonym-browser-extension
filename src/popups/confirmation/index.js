5c7daca869f30ae9f2bf030376d8ffac7b632742
calebtuttle <1calebtuttle@gmail.com>
2022-08-15 15:17:55 -0400

    Clear latestHoloMessage when user closes popup window without confirming

--- src/popups/confirmation/index.js
+++ src/popups/confirmation/index.js
@@ -2,6 +2,12 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App";
 
+// On close, tell background script to clear latestHoloMessage
+window.addEventListener("beforeunload", (event) => {
+  const message = { message: "denyCredentials" };
+  chrome.runtime.sendMessage(message);
+});
+
 const container = document.getElementById("root");
 const root = createRoot(container);
 root.render(<App />);
