ccf4a65443da830657c6091175092792fb567d2a
calebtuttle <1calebtuttle@gmail.com>
2022-08-17 18:01:36 -0400

    Use react-router for routing in confirmation popup

--- src/frontend/popups/confirmation/App.js
+++ src/frontend/popups/confirmation/App.js
@@ -1,64 +1,17 @@
-import React, { useState } from "react";
+import React from "react";
+import { MemoryRouter } from "react-router-dom";
 import HoloLogo from "../../img/Holo-Logo.png";
-import PasswordLogin from "../../components/atoms/PasswordLogin";
-import ConfirmCredentials from "../../components/molecules/ConfirmCredentials";
-import Success from "../../components/atoms/Success";
-
-const successMessage =
-  "Your credentials have been encrypted and stored. " +
-  "You can now generate zero knowledge proofs of identity.";
+import AppRoutes from "./AppRoutes";
 
 function App() {
-  const [loginIsVisible, setLoginIsVisible] = useState(true);
-  const [credsContainerIsVisible, setCredsContainerIsVisible] = useState(false);
-  const [credentials, setCredentials] = useState();
-  const [credsConfirmed, setCredsConfirmed] = useState();
-
-  async function handleLoginSuccess() {
-    const credentials = await requestCredentials();
-    setLoginIsVisible(false);
-    setCredentials(credentials);
-    setCredsContainerIsVisible(true);
-  }
-
-  function requestCredentials() {
-    return new Promise((resolve) => {
-      const message = { message: "getHoloLatestMessage" };
-      const callback = (resp) => resolve(resp.credentials);
-      chrome.runtime.sendMessage(message, callback);
-    });
-  }
-
-  function handleConfirmation() {
-    const message = { message: "confirmCredentials" };
-    const callback = (resp) => {
-      setCredsContainerIsVisible(false);
-      setCredsConfirmed(true);
-    };
-    chrome.runtime.sendMessage(message, callback);
-  }
-
   return (
     <>
       <div>
         <img src={HoloLogo} style={{ height: "25px" }} />
         <div style={{ margin: "10px" }}></div>
-        {loginIsVisible && <PasswordLogin onLoginSuccess={handleLoginSuccess} />}
-        {credsContainerIsVisible && (
-          <ConfirmCredentials
-            credentials={credentials}
-            onConfirmation={handleConfirmation}
-          />
-        )}
-        {credsConfirmed && (
-          <div style={{ marginTop: "150px" }}>
-            <Success
-              message={successMessage}
-              onExit={window.close}
-              exitButtonText="Close"
-            />
-          </div>
-        )}
+        <MemoryRouter>
+          <AppRoutes />
+        </MemoryRouter>
       </div>
     </>
   );
---
+++ src/frontend/popups/confirmation/AppRoutes.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import { Routes, Route, useNavigate } from "react-router-dom";
+import PasswordLogin from "../../components/atoms/PasswordLogin";
+import ConfirmCredentials from "../../components/molecules/ConfirmCredentials";
+import Success from "../../components/atoms/Success";
+
+const successMessage =
+  "Your credentials have been encrypted and stored. " +
+  "You can now generate zero knowledge proofs of identity.";
+
+function AppRoutes() {
+  const [credentials, setCredentials] = useState();
+  const navigate = useNavigate();
+
+  async function handleLoginSuccess() {
+    const credentials = await requestCredentials();
+    setCredentials(credentials);
+    navigate("/confirm", { replace: true });
+  }
+
+  function requestCredentials() {
+    return new Promise((resolve) => {
+      const message = { message: "getHoloLatestMessage" };
+      const callback = (resp) => resolve(resp.credentials);
+      chrome.runtime.sendMessage(message, callback);
+    });
+  }
+
+  function handleConfirmation() {
+    const message = { message: "confirmCredentials" };
+    const callback = (resp) => {
+      navigate("/success", { replace: true });
+    };
+    chrome.runtime.sendMessage(message, callback);
+  }
+
+  return (
+    <>
+      <Routes>
+        <Route
+          path="/"
+          element={<PasswordLogin onLoginSuccess={handleLoginSuccess} />}
+        />
+        <Route
+          path="/confirm"
+          element={
+            <ConfirmCredentials
+              credentials={credentials}
+              onConfirmation={handleConfirmation}
+            />
+          }
+        />
+        <Route
+          path="/success"
+          element={
+            <div style={{ marginTop: "150px" }}>
+              <Success
+                message={successMessage}
+                onExit={window.close}
+                exitButtonText="Close"
+              />
+            </div>
+          }
+        />
+      </Routes>
+    </>
+  );
+}
+
+export default AppRoutes;
