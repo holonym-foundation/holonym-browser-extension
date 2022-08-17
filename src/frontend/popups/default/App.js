271252c75dad9998f5dbbdad393463c0bbf0ea72
calebtuttle <1calebtuttle@gmail.com>
2022-08-16 20:04:03 -0400

    Signal whether user has credentials after login in default popup

--- src/frontend/popups/default/App.js
+++ src/frontend/popups/default/App.js
@@ -1,20 +1,36 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import HoloLogo from "../../img/Holo-Logo.png";
 import LandingPage from "../../components/LandingPage";
 
 function App() {
   const [landingPageIsVisible, setLandingPageIsVisible] = useState(true);
+  const [hasCredentials, setHasCredentials] = useState(false);
 
   async function handleLoginSuccess() {
     setLandingPageIsVisible(false);
   }
+
+  useEffect(() => {
+    function requestCredentials() {
+      return new Promise((resolve) => {
+        const message = { message: "getHoloCredentials" };
+        const callback = (resp) => resolve(resp.credentials);
+        chrome.runtime.sendMessage(message, callback);
+      });
+    }
+    requestCredentials().then((credentials) => setHasCredentials(!!credentials));
+  }, [landingPageIsVisible]);
 
   return (
     <>
       <div style={{ margin: "5px" }}>
         <img src={HoloLogo} style={{ height: "25px" }} />
         <div style={{ margin: "10px" }}></div>
-        {landingPageIsVisible && <LandingPage onLoginSuccess={handleLoginSuccess} />}
+        {landingPageIsVisible ? (
+          <LandingPage onLoginSuccess={handleLoginSuccess} />
+        ) : (
+          <p>Possess credentials: {hasCredentials.toString()}</p>
+        )}
       </div>
     </>
   );
