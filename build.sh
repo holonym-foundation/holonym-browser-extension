0f764115182883f21645932da626e935cf4cd0d4
calebtuttle <1calebtuttle@gmail.com>
2022-08-26 15:26:02 -0400

    Add icons

--- build.sh
+++ build.sh
@@ -6,4 +6,7 @@ cp ./src/frontend/styles/*.css ./dist
 cp ./src/frontend/popups/default/popup.html ./dist/default_popup.html
 cp ./src/frontend/popups/confirmation/popup.html ./dist/confirmation_popup.html
 
+# copy icons
+cp ./src/frontend/img/*Holo-Logo-no-text-w-bg* ./dist
+
 rollup --config rollup.config.js
