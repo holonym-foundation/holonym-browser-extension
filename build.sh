ae679e67398bcc447de4553e13aa55d5a07246e0
calebtuttle <1calebtuttle@gmail.com>
2022-08-26 19:27:02 -0400

    Add zip command to build script

--- build.sh
+++ build.sh
@@ -6,7 +6,11 @@ cp ./src/frontend/styles/*.css ./dist
 cp ./src/frontend/popups/default/popup.html ./dist/default_popup.html
 cp ./src/frontend/popups/confirmation/popup.html ./dist/confirmation_popup.html
 
-# copy icons
-cp ./src/frontend/img/*Holo-Logo-no-text-w-bg* ./dist
+cp ./src/frontend/img/*Holo-Logo-no-text-w-bg* ./dist # copy icons
 
+# Bundle
 rollup --config rollup.config.js
+
+# zip for production
+printf "\nzipping ./dist\n"
+zip -r ./prod-materials/dist.zip ./dist
