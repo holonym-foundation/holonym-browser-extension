14bc531373d253acbf6a308c409ccad53238eb3f
calebtuttle <1calebtuttle@gmail.com>
2022-09-11 11:28:55 -0400

    Fix display of proof items for confirmation of proof storage

--- src/frontend/components/molecules/ConfirmProof.js
+++ src/frontend/components/molecules/ConfirmProof.js
@@ -1,13 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 function ConfirmProof({ proof, onConfirmation }) {
+  const [proofItems, setProofItems] = useState();
+
+  useEffect(() => {
+    if (!proof || typeof proof != "object") return;
+    setProofItems(Object.keys(proof));
+  }, []);
+
   return (
     <>
       <div style={{ textAlign: "center" }}>
         <h2 className="header-base">Confirm Proof</h2>
-        <p className="small-paragraph">Confirm storage of this proof.</p>
+        <p className="small-paragraph">Confirm storage of these proof items.</p>
       </div>
-      <p>{proof}</p>
+      {proofItems &&
+        proofItems.length > 0 &&
+        proofItems.map((item, index) => <p key={index}>{item}</p>)}
       <div style={{ marginTop: "10px" }}>
         <button
           type="submit"
