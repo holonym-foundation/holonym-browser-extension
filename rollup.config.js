6fda9214ef4ce9d49b1217df9b817e0b034617a6
calebtuttle <1calebtuttle@gmail.com>
2022-09-10 22:31:48 -0400

    Change ENVIRONMENT to NODE_ENV

--- rollup.config.js
+++ rollup.config.js
@@ -42,7 +42,7 @@ export default [
         preferBuiltins: false,
       }),
       replace({
-        "process.env.ENVIRONMENT": ENVIRONMENT,
+        "process.env.NODE_ENV": ENVIRONMENT,
         preventAssignment: true,
       }),
       commonjs(),
@@ -65,7 +65,7 @@ export default [
         preferBuiltins: false,
       }),
       replace({
-        "process.env.ENVIRONMENT": ENVIRONMENT,
+        "process.env.NODE_ENV": ENVIRONMENT,
         preventAssignment: true,
       }),
       babel({
@@ -90,7 +90,7 @@ export default [
         preferBuiltins: false,
       }),
       replace({
-        "process.env.ENVIRONMENT": ENVIRONMENT,
+        "process.env.NODE_ENV": ENVIRONMENT,
         preventAssignment: true,
       }),
       babel({
