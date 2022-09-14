47725012d7ecef5a0ef6d91320da505f0f624547
nanaknihal <nanaknihal@gmail.com>
2022-09-14 17:03:18 -0400

    reverted landing page mostly

--- src/frontend/components/LandingPage.js
+++ src/frontend/components/LandingPage.js
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from "react";
 import SetPassword from "./molecules/SetPassword";
 import PasswordLogin from "./atoms/PasswordLogin";
 import ChangePassword from "./atoms/ChangePassword";
-import { sleep } from "../../background/utils";
 
 function LandingPage({ onLoginSuccess }) {
   const [registered, setRegistered] = useState(false);
@@ -10,26 +9,20 @@ function LandingPage({ onLoginSuccess }) {
   // TODO: Fix login! It's not working
 
   useEffect(() => {
-    async function getIsRegistered() {
-      let numAttempts = 0;
-      // try 50 times in case of port closed error
-      while (numAttempts < 50) {
-        console.log("attempt ", numAttempts)
-        const r = null;
-        try {
-          const callback = (resp) => console.log("response was ", resp);
-          chrome.runtime.sendMessage({command: "holoGetIsRegistered"}, callback);
-        } catch (err) {}
-        await sleep(50);
-        numAttempts += 1;
-      }
+    function getIsRegistered() {
+      return new Promise((resolve) => {
+        const message = {
+          command: "holoGetIsRegistered",
+        };
+        const callback = (resp) => resolve(resp.isRegistered);
+        chrome.runtime.sendMessage(message, callback);
+      });
     }
-    getIsRegistered().then((val) => {console.log(val); setRegistered(val)});
+    getIsRegistered().then((val) => setRegistered(val));
   }, []);
 
   return (
     <>
-      {/* <p>registered ? {registered} </p> */}
       {!registered ? (
         <SetPassword onAccountCreated={onLoginSuccess} />
       ) : (
