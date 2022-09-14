55985e8c373c2a6db6ff1ff1b0d7b905317d149a
calebtuttle <1calebtuttle@gmail.com>
2022-09-14 11:54:26 -0400

    Fix bug where credentials in default popup would not always be retrieved

--- src/frontend/popups/default/AppRoutes.js
+++ src/frontend/popups/default/AppRoutes.js
@@ -3,6 +3,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import LandingPage from "../../components/LandingPage";
 import Credentials from "../../components/atoms/Credentials";
 import ResetAccount from "../../components/molecules/ResetAccount";
+import { sleep } from "../../../background/utils";
 
 function AppRoutes() {
   const [loggedIn, setLoggedIn] = useState(false);
@@ -17,13 +18,30 @@ function AppRoutes() {
   useEffect(() => {
     if (loggedIn) return;
     function requestCredentials() {
-      return new Promise((resolve) => {
+      return new Promise((resolve, reject) => {
         const message = { command: "getHoloCredentials" };
-        const callback = (resp) => resolve(resp.credentials);
+        const callback = (resp) => {
+          if (!resp) reject();
+          resolve(resp.credentials);
+        };
         chrome.runtime.sendMessage(message, callback);
       });
     }
-    requestCredentials().then((credentials) => setCredentials(credentials));
+    async function getAndSetCredentials() {
+      let numAttempts = 0;
+      // try 50 times in case of port closed error
+      while (numAttempts < 50) {
+        try {
+          const credentials = await requestCredentials();
+          if (!credentials) continue;
+          setCredentials(credentials);
+          break;
+        } catch (err) {}
+        await sleep(50);
+        numAttempts += 1;
+      }
+    }
+    getAndSetCredentials();
   }, [loggedIn]);
 
   return (
