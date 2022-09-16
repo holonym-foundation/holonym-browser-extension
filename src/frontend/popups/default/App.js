4cc17b252d70464f772e218fca0eb56b16bc0da7
nanaknihal <nanaknihal@gmail.com>
2022-09-16 00:07:56 -0400

    nicer margin on logo

--- src/frontend/popups/default/App.js
+++ src/frontend/popups/default/App.js
@@ -1,14 +1,13 @@
 import React from "react";
 import { MemoryRouter } from "react-router-dom";
-import HoloLogo from "../../img/Holo-Logo.png";
+import HoloLogo from "../../components/atoms/HoloLogo";
 import AppRoutes from "./AppRoutes";
 
 function App() {
   return (
     <>
       <div style={{ margin: "5px" }}>
-        <img src={HoloLogo} style={{ height: "25px" }} />
-        <div style={{ margin: "10px" }}></div>
+        <HoloLogo />
         <MemoryRouter>
           <AppRoutes />
         </MemoryRouter>
