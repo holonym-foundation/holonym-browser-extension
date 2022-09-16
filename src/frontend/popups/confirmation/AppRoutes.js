0f9494b71e026dbaccdcd50f340ed8aaef0906e4
nanaknihal <nanaknihal@gmail.com>
2022-09-16 10:10:02 -0400

    made and commented out 'testing' credentials and route

--- src/frontend/popups/confirmation/AppRoutes.js
+++ src/frontend/popups/confirmation/AppRoutes.js
@@ -108,9 +108,37 @@ function AppRoutes() {
     window.close();
   }
 
+  const testCredentialsDeleteMe = {
+    firstName: "Vitalik",
+    lastName: "Buterin",
+    middleInitial: "",
+    countryCode: 0,
+    streetAddr1: "6969 Second Street",
+    streetAddr2: "",
+    city: "Los Angeles",
+    subdivision: "",
+    postalCode: "696969",
+    completedAt: "1234",
+    birthdate: "06/09/1969",
+  };
+  
+
   return (
     <>
       <Routes>
+      {
+      /* TESTING ROUTE (so can see other routes at "/" instead without having to organically navigate to them 
+      <Route
+          path="/"
+          element={<div style={{ marginTop: "150px" }}>
+          <Success
+            message={credsConfSuccessMessage}
+            onExit={onConfirmCredsContinue}
+            exitButtonText="Continue"
+          />
+        </div>}
+        /> 
+        */}
         <Route
           path="/"
           element={<LandingPage onLoginSuccess={handleLoginSuccess} />}
