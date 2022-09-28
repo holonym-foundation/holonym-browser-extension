93013d12b9c73aedefb6f8adf4e968eaa0619c9b
calebtuttle <1calebtuttle@gmail.com>
2022-09-28 16:48:35 -0400

    Add and reorganize tests. Use chai

--- test/test.js
+++ test/test.js
@@ -1,20 +1,24 @@
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 import puppeteer from "puppeteer";
-import assert from "assert";
-import { sendMessage } from "./utils.js";
+// import assert from "assert";
+import chai from "chai";
+import { sendMessage, encrypt } from "./utils.js";
+
+const { expect } = chai;
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const pathToExtension = `${__dirname}/../dist`;
 
-const devtools = false;
+// NOTE: frontendUrl must be either "https://app.holonym.id" or "http://localhost:3002"
+// Use "http://localhost:3002" if testing without internet connection
+const frontendUrl = "https://app.holonym.id";
 
 async function initialize() {
   const browser = await puppeteer.launch({
-    // headless: "chrome",
     headless: false,
-    devtools: devtools,
+    devtools: false,
     args: [
       `--disable-extensions-except=${pathToExtension}`,
       `--load-extension=${pathToExtension}`,
@@ -57,7 +61,7 @@ async function test() {
 
   const extPage = await browser.newPage();
   const defaultPopupUrl = `chrome-extension://${extensionId}/default_popup.html`;
-  await extPage.goto(defaultPopupUrl, { waitUntil: "networkidle0" }); // waitUntil: 'load' | 'domcontentloaded' | 'networkidle0' | 'networkidle2';
+  await extPage.goto(defaultPopupUrl, { waitUntil: "networkidle0" });
   await extPage.bringToFront();
   const initAccForm = await extPage.$("#initialize-account-form");
   // const passwordInput = await extPage.$("#initialize-account-form");
@@ -90,7 +94,7 @@ async function holoGetIsRegistered() {
 
   const extPage = await browser.newPage();
   const defaultPopupUrl = `chrome-extension://${extensionId}/default_popup.html`;
-  await extPage.goto(defaultPopupUrl, { waitUntil: "networkidle0" }); // waitUntil: 'load' | 'domcontentloaded' | 'networkidle0' | 'networkidle2';
+  await extPage.goto(defaultPopupUrl, { waitUntil: "networkidle0" });
   await extPage.bringToFront();
   const windowKeys = JSON.parse(
     await extPage.evaluate(() => JSON.stringify(Object.keys(window.chrome.runtime)))
@@ -106,7 +110,7 @@ async function holoGetIsRegistered() {
   const result = await sendMessage(extPage, extensionId, payload);
   console.log("result...");
   console.log(result);
-  assert.equal(result.isRegistered, false);
+  // assert.equal(result.isRegistered, false);
 
   // const initAccForm = await extPage.$("#initialize-account-form");
   // const pwInput = await initAccForm.$(".text-field");
@@ -122,17 +126,14 @@ async function holoGetIsRegistered() {
 // test();
 // holoGetIsRegistered();
 
-const allowedPopupCommands = [
-  "getHoloLatestMessage",
-  "getHoloCredentials",
-  "confirmCredentials",
-  "denyCredentials",
-  "confirmShareCredentials",
-  "closingHoloCredentialsConfirmationPopup",
-  "closingHoloShareCredsConfirmationPopup",
-];
-
-describe("Messages: popup script -> service worker", async () => {
+/**
+ * The following tests are separates into "atomic" and "interactive" tests.
+ * Atomic messages are messages that, upon being called once, have an observable
+ * affect on application state.
+ * A set of interactive messages is a set of messages that have an observable affect
+ * on application state only if the whole set is sent.
+ */
+describe("", async () => {
   let browser;
   let serviceWorkerTarget;
   let extensionId;
@@ -147,84 +148,148 @@ describe("Messages: popup script -> service worker", async () => {
     extensionId = initVals.extensionId;
     defaultPopupPage = await browser.newPage();
     const defaultPopupUrl = `chrome-extension://${extensionId}/default_popup.html`;
-    await defaultPopupPage.goto(defaultPopupUrl, { waitUntil: "networkidle0" }); // waitUntil: 'load' | 'domcontentloaded' | 'networkidle0' | 'networkidle2';
+    await defaultPopupPage.goto(defaultPopupUrl, { waitUntil: "networkidle0" });
     await defaultPopupPage.bringToFront();
   });
 
   after(async () => {
-    await browser.close();
+    browser.close();
   });
 
-  describe("holoGetIsRegistered", async () => {
-    it("Should return false before user has registered", async () => {
-      const payload = { command: "holoGetIsRegistered" };
-      const result = await sendMessage(defaultPopupPage, extensionId, payload);
-      assert.equal(result.isRegistered, false);
+  describe("Atomic messages from popup to service worker", async () => {
+    describe("holoGetIsRegistered", async () => {
+      it("Should return false before user has registered", async () => {
+        const payload = { command: "holoGetIsRegistered" };
+        const result = await sendMessage(defaultPopupPage, extensionId, payload);
+        expect(result.isRegistered).to.equal(false);
+      });
+    });
+
+    describe("holoInitializeAccount", async () => {
+      it("Should register the user, given a password", async () => {
+        // Call holoInitializeAccount
+        const payload1 = {
+          command: "holoInitializeAccount",
+          password: validPassword,
+        };
+        const result1 = await sendMessage(defaultPopupPage, extensionId, payload1);
+        // Check registration status
+        const payload2 = { command: "holoGetIsRegistered" };
+        const result2 = await sendMessage(defaultPopupPage, extensionId, payload2);
+        expect(result2.isRegistered).to.equal(true);
+      });
+    });
+
+    describe("holoPopupLogin", async () => {
+      it("Should fail to login, given an incorrect password", async () => {
+        const payload = {
+          command: "holoPopupLogin",
+          password: "wrong-password",
+        };
+        const result = await sendMessage(defaultPopupPage, extensionId, payload);
+        expect(result.success).to.equal(false);
+      });
+
+      it("Should login, given the correct password", async () => {
+        const payload = {
+          command: "holoPopupLogin",
+          password: validPassword,
+        };
+        const result = await sendMessage(defaultPopupPage, extensionId, payload);
+        expect(result.success).to.equal(true);
+      });
+    });
+
+    describe("holoChangePassword", async () => {
+      it("Should fail to change the user's password, given an incorrect oldPassword", async () => {
+        const payload = {
+          command: "holoChangePassword",
+          oldPassword: "invalid-password",
+          newPassword: "new-password",
+        };
+        const result = await sendMessage(defaultPopupPage, extensionId, payload);
+        expect(result.success).to.equal(false);
+      });
+
+      it("Should change the user's password, given the correct oldPassword", async () => {
+        const payload1 = {
+          command: "holoChangePassword",
+          oldPassword: validPassword,
+          newPassword: "new-password",
+        };
+        const result1 = await sendMessage(defaultPopupPage, extensionId, payload1);
+        expect(result1.success).to.equal(true);
+        // Change pw back to validPassword
+        const payload2 = {
+          command: "holoChangePassword",
+          oldPassword: "new-password",
+          newPassword: validPassword,
+        };
+        const result2 = await sendMessage(defaultPopupPage, extensionId, payload2);
+        expect(result2.success).to.equal(true);
+      });
     });
   });
 
-  describe("holoInitializeAccount", async () => {
-    it("Should register the user, given a password", async () => {
-      // Call holoInitializeAccount
-      const payload1 = {
-        command: "holoInitializeAccount",
-        password: validPassword,
-      };
-      const result1 = await sendMessage(defaultPopupPage, extensionId, payload1);
-      // Check registration status
-      const payload2 = { command: "holoGetIsRegistered" };
-      const result2 = await sendMessage(defaultPopupPage, extensionId, payload2);
-      assert.equal(result2.isRegistered, true);
+  describe("Atomic messages from frontend to service worker", async () => {
+    let frontendPage;
+
+    before(async () => {
+      frontendPage = await browser.newPage();
+      await frontendPage.goto(frontendUrl, { waitUntil: "networkidle0" });
+      await frontendPage.bringToFront();
+    });
+
+    describe("holoGetIsRegistered", async () => {
+      it("Should return true, given that the user has registered", async () => {
+        const payload = { command: "holoGetIsRegistered" };
+        const result = await sendMessage(frontendPage, extensionId, payload);
+        expect(result.isRegistered).to.equal(true);
+      });
+    });
+
+    describe("getHoloPublicKey", async () => {
+      it("Should return a JWK object that can be used to encrypt a message", async () => {
+        const payload = { command: "getHoloPublicKey" };
+        const publicKey = await sendMessage(frontendPage, extensionId, payload);
+        expect(publicKey).to.be.an("object");
+        expect(async () => await encrypt(publicKey, "test-message")).to.not.throw();
+        const encryptedMessage = await encrypt(publicKey, "test-message");
+        expect(encryptedMessage).to.be.a("string");
+      });
     });
   });
 
-  describe("holoPopupLogin", async () => {
-    it("Should fail to login, given an incorrect password", async () => {
-      const payload = {
-        command: "holoPopupLogin",
-        password: "wrong-password",
-      };
-      const result = await sendMessage(defaultPopupPage, extensionId, payload);
-      assert.equal(result.success, false);
-    });
+  const allowedPopupCommands = [
+    "getHoloLatestMessage",
+    "getHoloCredentials",
+    "confirmCredentials",
+    "denyCredentials",
+    "confirmShareCredentials",
+    "closingHoloCredentialsConfirmationPopup",
+    "closingHoloShareCredsConfirmationPopup",
+  ];
 
-    it("Should login, given the correct password", async () => {
-      const payload = {
-        command: "holoPopupLogin",
-        password: validPassword,
-      };
-      const result = await sendMessage(defaultPopupPage, extensionId, payload);
-      assert.equal(result.success, true);
-    });
-  });
+  const allowedWebPageCommands = ["getHoloCredentials", "setHoloCredentials"];
 
-  describe("holoChangePassword", async () => {
-    it("Should fail to change the user's password, given an incorrect oldPassword", async () => {
-      const payload = {
-        command: "holoChangePassword",
-        oldPassword: "invalid-password",
-        newPassword: "new-password",
-      };
-      const result = await sendMessage(defaultPopupPage, extensionId, payload);
-      assert.equal(result.success, false);
-    });
-
-    it("Should change the user's password, given the correct oldPassword", async () => {
-      const payload1 = {
-        command: "holoChangePassword",
-        oldPassword: validPassword,
-        newPassword: "new-password",
-      };
-      const result1 = await sendMessage(defaultPopupPage, extensionId, payload1);
-      assert.equal(result1.success, true);
-      // Change pw back to validPassword
-      const payload2 = {
-        command: "holoChangePassword",
-        oldPassword: "new-password",
-        newPassword: validPassword,
-      };
-      const result2 = await sendMessage(defaultPopupPage, extensionId, payload2);
-      assert.equal(result2.success, true);
+  describe("Interactive messages", async () => {
+    describe("getHoloLatestMessage", async () => {
+      it("Should fail to login, given an incorrect password", async () => {
+        // Test the following flow:
+        // frontend sends credentials ->
+        // user confirms [or denies] storage of credentials ->
+        // chrome storage state changes (both credentials and latest message)
+        const nonExtensionPage = await browser.newPage();
+        await nonExtensionPage.goto("https://app.holonym.id", {
+          waitUntil: "networkidle0",
+        });
+        await nonExtensionPage.bringToFront();
+        const payload = {
+          command: "setHoloCredentials",
+          credentials: { key: "value" },
+        };
+        sendMessage(nonExtensionPage, extensionId, payload);
+      });
     });
   });
 });
--- test/utils.js
+++ test/utils.js
@@ -1,3 +1,9 @@
+import { webcrypto } from "crypto";
+
+// Max length of encrypt-able string using RSA-OAEP with SHA256 where
+// modulusLength == 4096: 446 characters.
+const maxEncryptableLength = 446;
+
 /**
  * Wrapper for chrome.runtime.sendMessage. Timeout after 5 seconds
  * @param {Puppeteer.Page} page The page from which the message is sent
@@ -24,4 +30,44 @@ export async function sendMessage(page, extensionId, payload) {
     );
     resolve(result);
   });
+}
+/**
+ * @param {SubtleCrypto.JWK} publicKey
+ * @param {string} message
+ * @returns {Promise<string>} Encrypted message
+ */
+export async function encrypt(publicKey, message) {
+  const algo = {
+    name: "RSA-OAEP",
+    modulusLength: 4096,
+    publicExponent: new Uint8Array([1, 0, 1]),
+    hash: "SHA-256",
+  };
+  let args = ["jwk", publicKey, algo, false, ["encrypt"]];
+  const pubKeyAsCryptoKey = await webcrypto.subtle.importKey(...args);
+  const encoder = new TextEncoder();
+  const encodedMessage = encoder.encode(message);
+  args = ["RSA-OAEP", pubKeyAsCryptoKey, encodedMessage];
+  const encryptedMessage = await webcrypto.subtle.encrypt(...args);
+  return JSON.stringify(Array.from(new Uint8Array(encryptedMessage)));
+}
+
+export async function encryptForExtension(page, extensionId, message) {
+  const payload = { command: "getHoloPublicKey" };
+  const encryptionKey = await sendMessage(page, extensionId, payload);
+
+  const stringifiedMsg = JSON.stringify(message);
+  const usingSharding = stringifiedMsg.length > maxEncryptableLength;
+  let encryptedMessage; // array<string> if sharding, string if not sharding
+  if (usingSharding) {
+    encryptedMessage = [];
+    for (let i = 0; i < stringifiedMsg.length; i += maxEncryptableLength) {
+      const shard = stringifiedMsg.substring(i, i + maxEncryptableLength);
+      const encryptedShard = await encrypt(encryptionKey, shard);
+      encryptedMessage.push(encryptedShard);
+    }
+  } else {
+    encryptedMessage = await encrypt(encryptionKey, stringifiedMsg);
+  }
+  return { encryptedMessage: encryptedMessage, sharded: usingSharding };
 }
